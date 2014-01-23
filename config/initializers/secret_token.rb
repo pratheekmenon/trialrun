53a00eb337654e5003467d2366aa516f234cec35
Pratheek Menon <pr62@duke.edu>
2014-01-23 13:48:16 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Neuroplus::Application.config.secret_key_base = '48e03c9f4dc8252c0b95437e9b36b600f963af26ce0b8ec86353b8c5eff23aeeab6bc791c04451e934bd0e8425a31f7956fa6980112692a87cebb40cbd1db0d8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Neuroplus::Application.config.session_store :cookie_store, key: '_neuroplus_session'
